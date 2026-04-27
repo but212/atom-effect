3264cd40933d06defcc29a8f18c3d395457bb8c8
but212 <wjddlf211@naver.com>
2026-04-28 02:58:09 +0900

    fix: correctful config

--- packages/jquery/vitest.config.ts
+++ packages/jquery/vitest.config.ts
@@ -1,7 +1,7 @@
-import { createVitestConfig } from '@but212/atom-effect-configs';
+import { defineVitestConfig } from '@but212/atom-effect-configs';
 import { playwright } from '@vitest/browser-playwright';
 
-export default createVitestConfig(import.meta.dirname, {
+export default defineVitestConfig(import.meta.dirname)({
   test: {
     setupFiles: ['./__tests__/setup.ts'],
     browser: {
