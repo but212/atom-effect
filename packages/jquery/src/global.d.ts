efd8988fa6c27b3a60a7bfcdd193879dfdd6f0ca
but212 <wjddlf211@naver.com>
2026-06-13 14:22:00 +0900

    refactor: add generic defaults to atomBind

--- packages/jquery/src/global.d.ts
+++ packages/jquery/src/global.d.ts
@@ -889,7 +889,7 @@ declare global {
      * });
      * ```
      */
-    atomBind<T, TText>(opts: BindingOptions<T, TText>): this;
+    atomBind<T = unknown, TText = unknown>(opts: BindingOptions<T, TText>): this;
 
     /**
      * Synchronizes an element's children with a reactive list source.
